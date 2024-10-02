601b88b42ee47b29790af98edb337daef2a7180f
Christoph Mayr-Dorn <c.mayr-dorn@jku.at>
2024-10-02 17:21:16 +0200

    first attempt of using owl in jena

---
+++ src/test/java/at/jku/isse/artifacteventstreaming/jena/OntologyModelTest.java
@@ -0,0 +1,44 @@
+package at.jku.isse.artifacteventstreaming.jena;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.apache.jena.ontapi.OntModelFactory;
+import org.apache.jena.ontapi.OntSpecification;
+import org.apache.jena.ontapi.model.OntClass;
+import org.apache.jena.ontapi.model.OntDataProperty;
+import org.apache.jena.ontapi.model.OntIndividual;
+import org.apache.jena.ontapi.model.OntModel;
+import org.apache.jena.ontapi.model.OntObjectProperty;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.vocabulary.XSD;
+import org.junit.jupiter.api.Test;
+
+class OntologyModelTest {
+
+	public static String NS = "http://at.jku.isse.jena#";
+	
+	@Test
+	void test() {
+		OntModel m = OntModelFactory.createModel( OntSpecification.OWL2_DL_MEM );
+		OntClass artifact = m.createOntClass(NS);
+		OntObjectProperty successorProp = m.createObjectProperty(NS+"successor");
+		successorProp.addDomain(artifact);
+		successorProp.addRange(artifact);
+		successorProp.addLabel("has successor");
+		
+		OntDataProperty keyProp = m.createDataProperty(NS+"key");
+		keyProp.addDomain(artifact);
+		keyProp.addRange(m.getDatatype(XSD.xstring));
+		
+		// lets create some instances:
+		OntIndividual art1 = artifact.createIndividual(NS+"art1");
+		OntIndividual art2 = artifact.createIndividual(NS+"art2");
+		
+		art1.addProperty(keyProp, "TESTKEY-1");
+		art2.addProperty(keyProp, "TESTKEY-2");
+		
+		//art1.addProperty(successorProp, art2);
+		
+	}
+
+}
