ef278cdfa6b08f7d9d0c55e52c7df30ffb4a2a6b
Christoph Mayr-Dorn <christoph.mayr-dorn@jku.at>
2024-10-04 14:23:20 +0200

    added first validation test

--- src/test/java/at/jku/isse/artifacteventstreaming/jena/OntologyModelTest.java
+++ src/test/java/at/jku/isse/artifacteventstreaming/jena/OntologyModelTest.java
@@ -7,9 +7,14 @@ import org.apache.jena.ontapi.OntSpecification;
 import org.apache.jena.ontapi.model.OntClass;
 import org.apache.jena.ontapi.model.OntDataProperty;
 import org.apache.jena.ontapi.model.OntIndividual;
+import org.apache.jena.ontapi.model.OntIndividual.Named;
 import org.apache.jena.ontapi.model.OntModel;
 import org.apache.jena.ontapi.model.OntObjectProperty;
+import org.apache.jena.rdf.model.InfModel;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Property;
+import org.apache.jena.reasoner.Reasoner;
+import org.apache.jena.reasoner.ReasonerRegistry;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.vocabulary.XSD;
@@ -22,26 +27,39 @@ class OntologyModelTest {
 	@Test
 	void test() {
 		OntModel m = OntModelFactory.createModel( OntSpecification.OWL2_DL_MEM );
-		OntClass artifact = m.createOntClass(NS+"artifact");		
-		OntObjectProperty successorProp = m.createObjectProperty(NS+"successor");
-		successorProp.addDomain(artifact);
-		successorProp.addRange(artifact);
+		OntClass artifactType = m.createOntClass(NS+"artifact");		
+		OntClass otherType = m.createOntClass(NS+"other");
+		otherType.addDisjointClass(artifactType);
+		
+		OntObjectProperty.Named successorProp = m.createObjectProperty(NS+"successor");
+		successorProp.addDomain(artifactType);
+		successorProp.addRange(artifactType);
 		successorProp.addLabel("has successor");
 		
 		OntDataProperty keyProp = m.createDataProperty(NS+"key");
-		keyProp.addDomain(artifact);
+		keyProp.addDomain(artifactType);
 		keyProp.addRange(m.getDatatype(XSD.xstring));
 		
 		// lets create some instances:
-		OntIndividual art1 = artifact.createIndividual(NS+"art1");
-		OntIndividual art2 = artifact.createIndividual(NS+"art2");
+		OntIndividual art1 = artifactType.createIndividual(NS+"art1");
+		OntIndividual art2 = artifactType.createIndividual(NS+"art2");
 		
 		art1.addProperty(keyProp, "TESTKEY-1");
 		art2.addProperty(keyProp, "TESTKEY-2");
 		
-		//art1.addProperty(successorProp, art2);
+		Named other1 = otherType.createIndividual(NS+"other1");
+		art1.addProperty(successorProp, art2);
+		art1.addProperty(successorProp, other1);
+					
+		
+		
 		RDFDataMgr.write(System.out, m, Lang.TURTLE) ;
-		Utils.printStream(artifact.declaredProperties());
+		Utils.printStream(artifactType.declaredProperties());
+		
+		Reasoner reasoner = ReasonerRegistry.getOWLMiniReasoner();
+		reasoner.bindSchema(m);
+		InfModel infmodel = ModelFactory.createInfModel(reasoner, m);
+		Utils.printValidation(infmodel);
 	}
 
 }
